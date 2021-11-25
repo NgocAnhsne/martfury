f27137ce4f8b5a96bdb59f742bc2390cf56229d3
ngocanhne <ngocanhne2402@gmail.com>
2021-11-25 14:16:25 +0700

    initial commit

---
+++ js.js
@@ -0,0 +1,11 @@
+document.onscroll = function(){
+    const scrollTop = window.scrollY
+    if(scrollTop > 68){
+        document.getElementById("header").style.backgroundColor="white"
+    }
+    else{
+        document.getElementById("header").style.backgroundColor="transparent"
+    }
+
+}
+console.log("akhduqwg")
